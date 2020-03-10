c5900dcfc644650e56c33bf5fb6014d1e6b3af5d
Suporte iClouds <suporte@iclouds.com.br>
2020-03-10 17:35:33 -0300

    feat: 🎸 Initialized mobile layout

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
+import media from 'styled-media-query';
 
 import { pxToRem } from '../../functions';
 
 export const Container = styled.div`
-  width: ${pxToRem(1366)};
+  max-width: ${pxToRem(1366)};
   margin: 0 auto;
+
+  ${media.lessThan('large')`
+    max-width: 1170px;
+  `}
 `;
