0b8042704032f497d13f772041b472e17bb157ab
fernando-meira <thomwork@outlook.com>
2020-03-12 17:44:48 -0300

    feat: 🎸 Accordion

---
+++ src/pages/Restaurants/components/Accordion/dataMock.js
@@ -0,0 +1,92 @@
+export default [
+  {
+    id: 1,
+    type: 'Almoços',
+    product: [
+      {
+        id: 1,
+        image:
+          'https://img.itdg.com.br/tdg/images/recipes/000/178/076/216464/216464_original.jpg?mode=crop&width=710&height=400l',
+        name: 'Lasanha',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        value: 19.9,
+        promotionalValue: 16.9,
+      },
+      {
+        id: 2,
+        image:
+          'https://p2.trrsf.com/image/fget/cf/460/0/images.terra.com/2018/09/28/feijoada-completa-tradicional.jpg',
+        name: 'Feijoada',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        value: 19.9,
+        promotionalValue: 15.9,
+      },
+      {
+        id: 3,
+        image:
+          'http://www.dicasdemulher.com.br/wp-content/uploads/2014/11/receitas-faceis-de-almoco.jpg',
+        name: 'Camarão flambado com purê de mandioquinha e quinua',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        value: 50.9,
+        promotionalValue: 25.9,
+      },
+      {
+        id: 4,
+        image:
+          'https://www.receiteria.com.br/wp-content/uploads/almo%C3%A7o-747x482.jpg',
+        name: 'Panqueca',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        value: 19.9,
+        promotionalValue: 10.9,
+      },
+    ],
+  },
+  {
+    id: 2,
+    type: 'Bebidas',
+    product: [
+      {
+        id: 1,
+        image:
+          'https://melhorcomsaude.com.br/wp-content/uploads/2016/11/bebidas-mais-saud%C3%A1veis-do-que-a-%C3%A1gua-suco-de-beterraba-500x232.jpg',
+        name: 'Suco de Beterraba',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        value: 10.9,
+        promotionalValue: 4.9,
+      },
+      {
+        id: 2,
+        image:
+          'https://melhorcomsaude.com.br/wp-content/uploads/2016/11/%C3%A1gua-coco-500x334.jpg',
+        name: 'Água de coco',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        value: 10.9,
+        promotionalValue: 4.9,
+      },
+      {
+        id: 3,
+        image:
+          'https://melhorcomsaude.com.br/wp-content/uploads/2016/11/bebidas-mais-saud%C3%A1veis-do-que-a-%C3%A1gua-suco-de-beterraba-500x232.jpg',
+        name: 'Chá Verde',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        value: 8.9,
+        promotionalValue: 3.9,
+      },
+    ],
+  },
+  {
+    id: 3,
+    type: 'Sobremesas',
+  },
+  {
+    id: 4,
+    type: 'Acompanhamento',
+  },
+];
--- src/pages/Restaurants/components/Accordion/index.js
+++ src/pages/Restaurants/components/Accordion/index.js
@@ -1,11 +1,5 @@
 import React, { useState, useEffect } from 'react';
 
-import lunch from '../../../../themes/assets/images/prato-de-restaurante-vegetariano@2x.png';
-
-import { Container, ItemRestaurant } from './styles';
-
-import 'react-accessible-accordion/dist/fancy-example.css';
-
 import {
   Accordion,
   AccordionItem,
@@ -13,33 +7,16 @@ import {
   AccordionItemButton,
   AccordionItemPanel,
 } from 'react-accessible-accordion';
+import lunch from '../../../../themes/assets/images/prato-de-restaurante-vegetariano@2x.png';
+
+import { Container, ItemRestaurant } from './styles';
+import mock from './dataMock';
 
 // Demo styles, see 'Styles' section below for some notes on use.
 import 'react-accessible-accordion/dist/fancy-example.css';
 
 export default function Example() {
-  const [items] = useState([
-    {
-      id: 1,
-      type: 'Almoços',
-    },
-    {
-      id: 2,
-      type: 'Bebidas',
-    },
-    {
-      id: 3,
-      type: 'Sobremesas',
-    },
-    {
-      id: 4,
-      type: 'Acompanhamento',
-    },
-  ]);
-
-  useEffect(() => {
-    console.log('Items', items);
-  }, []);
+  const data = mock;
 
   return (
     <Container>
@@ -47,52 +24,13 @@ export default function Example() {
         allowMultipleExpanded={() => true}
         allowZeroExpanded={() => true}
       >
-        {items.map(item => (
-          <AccordionItem key={item.id}>
+        {data.map(category => (
+          <AccordionItem key={category.id}>
             <AccordionItemHeading>
-              <AccordionItemButton>{item.type}</AccordionItemButton>
+              <AccordionItemButton>{category.type}</AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel>
               <div className="Panel">
-                <ItemRestaurant>
-                  <img src={lunch} alt="Almoço" />
-                  <div className="PlateDetais">
-                    <strong>Nome do Prato</strong>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do
-                    </p>
-
-                    <span>R$ 19,90</span>
-                  </div>
-                </ItemRestaurant>
-
-                <ItemRestaurant>
-                  <img src={lunch} alt="Almoço" />
-                  <div className="PlateDetais">
-                    <strong>Nome do Prato</strong>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do
-                    </p>
-
-                    <span>R$ 19,90</span>
-                  </div>
-                </ItemRestaurant>
-
-                <ItemRestaurant>
-                  <img src={lunch} alt="Almoço" />
-                  <div className="PlateDetais">
-                    <strong>Nome do Prato</strong>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do
-                    </p>
-
-                    <span>R$ 19,90</span>
-                  </div>
-                </ItemRestaurant>
-
                 <ItemRestaurant>
                   <img src={lunch} alt="Almoço" />
                   <div className="PlateDetais">
