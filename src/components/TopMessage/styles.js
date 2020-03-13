34f6951762abc6de47fe7a8d76bb0adef81fdea2
fernando-meira <thomwork@outlook.com>
2020-03-13 16:17:19 -0300

    refactor: 💡 TopMessage(Components)
    Moved Component to components folder

--- src/components/TopMessage/styles.js
+++ src/components/TopMessage/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import { pxToRem } from '../../../../functions';
+import { pxToRem } from '../../functions';
 
 export const Container = styled.div`
   display: flex;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Header from './Header';
 import Search from './Search';
+import TopMessage from './TopMessage';
 import RestaurantList from './RestaurantList';
 
-export { Header, Search, RestaurantList };
+export { Header, Search, TopMessage, RestaurantList };
