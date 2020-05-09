58c06bde56e1486c33a1464fb506795d27a20831
Fernando Meira <thomwork@outlook.com>
2020-05-08 22:13:43 -0300

    feat: 🎸 Build

---
+++ build/precache-manifest.88b40b9dbeffe6d2f11d57287fdf4ee5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b16d2bbab64e0a3f1ceeb2253ae0631",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d38a6f7c46997dbad8a4",
+    "url": "/static/css/2.772b860c.chunk.css"
+  },
+  {
+    "revision": "d38a6f7c46997dbad8a4",
+    "url": "/static/js/2.42678830.chunk.js"
+  },
+  {
+    "revision": "2d1f0ed21c5c95e3a2cb212dbd2653e2",
+    "url": "/static/js/2.42678830.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe7d0961e94ec6a62204",
+    "url": "/static/js/main.c4976e40.chunk.js"
+  },
+  {
+    "revision": "6c448cdc47fe486d3844",
+    "url": "/static/js/runtime-main.b5c18508.js"
+  },
+  {
+    "revision": "8a18923fe60c963f3630afd70d1b51e5",
+    "url": "/static/media/prato-de-restaurante-vegetariano-modal.8a18923f.png"
+  },
+  {
+    "revision": "935271f48da1d15d630a98ad96e7dd1c",
+    "url": "/static/media/vegan-restaurant-logo-design_1438-10@2x.935271f4.png"
+  }
+]);
